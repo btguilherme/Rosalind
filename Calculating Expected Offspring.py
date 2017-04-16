6142118f6a7de439fec7f5649b8de877b2888c81
Guilherme Camargo <btguilherme@msn.com>
2017-04-16 20:29:50 -0300

    ...

---
+++ Calculating Expected Offspring.py
@@ -0,0 +1,6 @@
+
+numbers = "19835 16449 18534 18400 19188 17029".split(' ')
+
+print (float(2) * (float(numbers[0]) * float(1) + float(numbers[1]) * float(1) +
+ float(numbers[2]) * float(1) + float(numbers[3]) * float(0.75) +
+ float(numbers[4]) * 0.5))
